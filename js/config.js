0138f5076de13e020c0b2d873be0e68427f36bd6
LauraLujan1452 <nh.lujan@hotmail.com>
2026-02-19 21:03:22 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_aEfz8aZs5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3vrmfvco01prprp2c4dtpni4md', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
